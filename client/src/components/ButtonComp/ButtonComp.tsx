68622bd95325c8ccc12b5052280fe339972815f3
Mehdi-Zafar <mehdizaffar0@gmail.com>
2026-07-28 02:51:06 +0500

    New props added to button

--- client/src/components/ButtonComp/ButtonComp.tsx
+++ client/src/components/ButtonComp/ButtonComp.tsx
@@ -2,21 +2,26 @@ import { Button } from "@material-tailwind/react";
 import { twMerge } from "tailwind-merge";
 
 export default function ButtonComp({
+  variant = "filled",
   onClick,
   className,
   label,
   type = "submit",
   loading,
+  disabled,
 }: InputProps) {
   return (
     <Button
+      variant={variant}
       onClick={onClick}
+      color="blue"
       type={type}
       className={twMerge(
         `flex w-full justify-center capitalize font-medium rounded-md bg-lightPrimary dark:bg-darkPrimary px-3 py-1.5 text-sm leading-6 text-white shadow-sm hover:bg-indigo-700`,
-        className
+        className,
       )}
       loading={!!loading}
+      disabled={loading || disabled}
     >
       {label}
     </Button>
@@ -29,4 +34,6 @@ interface InputProps {
   label: string;
   type?: "submit" | "button" | "reset";
   loading?: boolean;
+  disabled?: boolean;
+  variant?: "filled" | "gradient" | "text" | "outlined";
 }
