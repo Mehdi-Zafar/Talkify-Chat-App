1ca93f73d29afda15551f890b67fe367382cd95b
Mehdi-Zafar <mehdizaffar0@gmail.com>
2026-07-31 05:47:55 +0500

    Removed manual contracts

--- server/src/lib/models.ts
+++ server/src/lib/models.ts
@@ -25,36 +25,6 @@ export enum SocketEvent {
   CONNECT_ERROR = "connectError",
 }
 
-export class User {
-  id: number;
-  user_name: string;
-  email: string;
-  phone_number: string;
-  gender: string;
-  password: string;
-  verification_code: string;
-  verification_code_expiry: number;
-  image: string;
-}
-
 export type PublicUser = Prisma.usersGetPayload<{
   select: typeof PUBLIC_USER_FIELDS;
 }>;
-
-export class Chat {
-  id: number;
-  name: string | null;
-  members: number[];
-  creator_id: number;
-  isGroupChat: boolean;
-  memberDetails?: User[];
-  messages: Message[];
-}
-
-export class Message {
-  id: number;
-  content: string;
-  attachments: string[];
-  sender: User;
-  chat_id: number;
-}
