e53e002ee02818094b06a2a2ea96b65e810f648b
Mehdi-Zafar <mehdizaffar0@gmail.com>
2026-08-01 05:42:55 +0500

    Added chat store

--- client/src/zustand/index.ts
+++ client/src/zustand/index.ts
@@ -2,3 +2,4 @@ export { default as useThemeStore } from "./ThemeStore";
 export { default as useAuthStore } from "./AuthStore";
 export { default as useUserStore } from "./UserStore";
 export { default as useSocketStore } from "./SocketStore";
+export { default as useChatStore } from "./ChatStore";
