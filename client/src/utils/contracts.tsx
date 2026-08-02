31b25d49726d8f79425bef2cb8d25d4550c73769
Mehdi-Zafar <mehdizaffar0@gmail.com>
2026-08-03 02:15:15 +0500

    Made changes to add new private and group chat client component and fixed their issues

--- client/src/utils/contracts.tsx
+++ client/src/utils/contracts.tsx
@@ -31,6 +31,17 @@ export class Chat {
   } | null;
 }
 
+export class CreateGroupChatRequest {
+  name: string = "";
+  isGroupChat: boolean = true;
+  members: number[] = [];
+}
+
+export class CreateChatRequest {
+  member_id: number = 0;
+  isGroupChat: boolean = false;
+}
+
 export class Message {
   id: number;
   content: string;
